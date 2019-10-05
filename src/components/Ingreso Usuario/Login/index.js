28d8c2443df7adbb87e7e2db0fb0491cc098a622
cgdr <christian.diaz.gonzalo@gmail.com>
2019-10-04 22:34:49 -0600

    Codigo de logueo

--- src/components/Ingreso Usuario/Login/index.js
+++ src/components/Ingreso Usuario/Login/index.js
@@ -1,30 +1,65 @@
 import React, {Component} from 'react';
 import {Grid, Col, Row} from 'react-flexbox-grid';
 import TextField from '@material-ui/core/TextField';
+import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import FacebookLogin from './Facebook'
+import FacebookLogin from './Facebook';
 import './style.css';
 
 class Login extends Component {
 
+    constructor(){
+        super();
+        this.state = ({
+            email: null,
+            message: null,
+        })
+    }
+
+    siLogueo(data){
+        
+    }
+
+
+    noLogueo(){
+        console.log("No funciono");
+    }
+
     autenticaUsuario = () => {
-        console.log("Empezare a ejecutar api");
-        fetch('https://api-social-network-umg.herokuapp.com/')
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
-            }
-        )
-        .catch(console.log("No he podido conectarme"))
+        const txtUsuario = document.getElementById("txtUsuario");
+        const txtContrasena = document.getElementById("txtContrasena");
+        const data = {
+                        "email":txtUsuario.value,
+                        "password": txtContrasena.value
+                    };
+
+        fetch('https://api-social-network-umg.herokuapp.com/users/authenticate',
+                {
+                    method: 'POST', // or 'PUT'
+                    body: JSON.stringify(data), // data can be `string` or {object}!
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }
+        ).then(
+            this.setState ({
+                email: data.email,
+                message: data.message,
+            })
+            ,this.noLogueo);
     };
 
     componentDidMount() {
-        
     };
     
 
     render(){
+        const {email, message} = this.state;
+        console.log(email)
+        const LinkToTimeLine = React.forwardRef((props, ref) => 
+            <Link innerRef={ref} to='/timeline' {...props}></Link>)
 
         return (
             <Grid>
@@ -39,6 +74,7 @@ class Login extends Component {
                                     placeholder="Ingrese usuario"
                                     margin="normal"
                                     variant="outlined"
+                                    value="christian.diaz.gonzalo@gmail"
                                 />
                             </Col>
                         </Row>
@@ -52,6 +88,7 @@ class Login extends Component {
                                     margin="normal"
                                     variant="outlined"
                                     type="password"
+                                    value="coco"
                                 />
                             </Col>
                         </Row>
@@ -63,6 +100,7 @@ class Login extends Component {
                                 size="medium" 
                                 fullWidth
                                 onClick={this.autenticaUsuario}
+                                component={email?LinkToTimeLine:"no"}
                                 >
                                     Iniciar sesion
                                     <AccountCircleIcon className="iconoPersonal"
